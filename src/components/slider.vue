f88b6fe86e7c62ca39c1cdd4dfb2eeaefb2db9a5
nidakars <36996383+nidakars@users.noreply.github.com>
2020-12-05 22:44:23 +0300

    slider düzenlendi

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -10,9 +10,9 @@
           <div class="owl-stage-outer">
             <div
               class="owl-stage"
-              style="transform: translate3d(-8771px, 0px, 0px); transition: all 0.25s ease 0s; width: 18795px;"
+              style="transform: translate3d(-7680px, 0px, 0px); transition: all 0.25s ease 0s; width: 28800px;"
             >
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -23,7 +23,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -34,7 +34,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -45,7 +45,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -56,7 +56,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item" style="width: 1253px;">
+              <div class="owl-item active" style="width: 1920px;">
                 <div class="item">
                   <a
                     title=""
@@ -71,7 +71,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item" style="width: 1253px;">
+              <div class="owl-item" style="width: 1920px;">
                 <div class="item">
                   <a
                     title=""
@@ -86,7 +86,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item" style="width: 1253px;">
+              <div class="owl-item" style="width: 1920px;">
                 <div class="item">
                   <a
                     title=""
@@ -101,7 +101,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item active" style="width: 1253px;">
+              <div class="owl-item" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -112,7 +112,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item" style="width: 1253px;">
+              <div class="owl-item" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -123,7 +123,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item" style="width: 1253px;">
+              <div class="owl-item" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -134,7 +134,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item" style="width: 1253px;">
+              <div class="owl-item" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -145,7 +145,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a
                     title=""
@@ -160,7 +160,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a
                     title=""
@@ -175,7 +175,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a
                     title=""
@@ -190,7 +190,7 @@
                   </a>
                 </div>
               </div>
-              <div class="owl-item cloned" style="width: 1253px;">
+              <div class="owl-item cloned" style="width: 1920px;">
                 <div class="item">
                   <a title="" href="/yeni-232" class="fl col-12">
                     <img
@@ -215,13 +215,29 @@
 </template>
 
 <style scoped>
+.HomePage #content > .inner * {
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+}
+.owl-theme .owl-nav {
+  margin-top: 10px;
+}
 .owl-theme .owl-dots,
 .owl-theme .owl-nav {
   text-align: center;
   -webkit-tap-highlight-color: transparent;
 }
 .owl-theme .owl-nav {
-  margin-top: 10px;
+  margin: 0 !important;
+}
+.owl-theme .owl-nav {
+  position: absolute;
+  top: calc(50% - 15px);
+  left: 20px;
+  width: calc(100% - 40px);
+  height: 0;
+  margin-top: 0;
 }
 .owl-theme .owl-nav [class*="owl-"] {
   color: #fff;
@@ -232,92 +248,6 @@
   display: inline-block;
   cursor: pointer;
   border-radius: 3px;
-}
-.owl-theme .owl-nav [class*="owl-"]:hover {
-  background: #869791;
-  color: #fff;
-  text-decoration: none;
-}
-.owl-theme .owl-nav .disabled {
-  opacity: 0.5;
-  cursor: default;
-}
-.owl-theme .owl-nav.disabled + .owl-dots {
-  margin-top: 10px;
-}
-.owl-theme .owl-dots .owl-dot {
-  display: inline-block;
-  zoom: 1;
-}
-.owl-theme .owl-dots .owl-dot span {
-  width: 10px;
-  height: 10px;
-  margin: 5px 7px;
-  background: #d6d6d6;
-  display: block;
-  -webkit-backface-visibility: visible;
-  transition: opacity 0.2s ease;
-  border-radius: 30px;
-}
-.owl-theme .owl-dots .owl-dot.active span,
-.owl-theme .owl-dots .owl-dot:hover span {
-  background: #869791;
-}
-
-.owl-carousel,
-.owl-carousel .owl-item {
-  -webkit-tap-highlight-color: transparent;
-  position: relative;
-}
-.owl-carousel {
-  display: none;
-  width: 100%;
-  z-index: 1;
-}
-.owl-carousel .owl-stage {
-  position: relative;
-  -ms-touch-action: pan-Y;
-  -moz-backface-visibility: hidden;
-}
-.owl-carousel .owl-stage:after {
-  content: ".";
-  display: block;
-  clear: both;
-  visibility: hidden;
-  line-height: 0;
-  height: 0;
-}
-.owl-carousel .owl-stage-outer {
-  position: relative;
-  overflow: hidden;
-  -webkit-transform: translate3d(0, 0, 0);
-}
-.owl-carousel .owl-item,
-.owl-carousel .owl-wrapper {
-  -webkit-backface-visibility: hidden;
-  -moz-backface-visibility: hidden;
-  -ms-backface-visibility: hidden;
-  -webkit-transform: translate3d(0, 0, 0);
-  -moz-transform: translate3d(0, 0, 0);
-  -ms-transform: translate3d(0, 0, 0);
-}
-.owl-carousel .owl-item {
-  min-height: 1px;
-  float: left;
-  -webkit-backface-visibility: hidden;
-  -webkit-touch-callout: none;
-}
-.owl-carousel .owl-item img {
-  display: block;
-  width: 100%;
-}
-.owl-carousel .owl-dots.disabled,
-.owl-carousel .owl-nav.disabled {
-  display: none;
-}
-.no-js .owl-carousel,
-.owl-carousel.owl-loaded {
-  display: block;
 }
 .owl-carousel .owl-dot,
 .owl-carousel .owl-nav .owl-next,
@@ -330,131 +260,6 @@
   -ms-user-select: none;
   user-select: none;
 }
-.owl-carousel.owl-loading {
-  opacity: 0;
-  display: block;
-}
-.owl-carousel.owl-hidden {
-  opacity: 0;
-}
-.owl-carousel.owl-refresh .owl-item {
-  visibility: hidden;
-}
-.owl-carousel.owl-drag .owl-item {
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-.owl-carousel.owl-grab {
-  cursor: move;
-  cursor: grab;
-}
-.owl-carousel.owl-rtl {
-  direction: rtl;
-}
-.owl-carousel.owl-rtl .owl-item {
-  float: right;
-}
-.owl-carousel .animated {
-  animation-duration: 1s;
-  animation-fill-mode: both;
-}
-.owl-carousel .owl-animated-in {
-  z-index: 0;
-}
-.owl-carousel .owl-animated-out {
-  z-index: 1;
-}
-.owl-carousel .fadeOut {
-  animation-name: fadeOut;
-}
-@keyframes fadeOut {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-.owl-height {
-  transition: height 0.5s ease-in-out;
-}
-.owl-carousel .owl-item .owl-lazy {
-  opacity: 0;
-  transition: opacity 0.4s ease;
-}
-.owl-carousel .owl-item img.owl-lazy {
-  transform-style: preserve-3d;
-}
-.owl-carousel .owl-video-wrapper {
-  position: relative;
-  height: 100%;
-  background: #000;
-}
-.owl-carousel .owl-video-play-icon {
-  position: absolute;
-  height: 80px;
-  width: 80px;
-  left: 50%;
-  top: 50%;
-  margin-left: -40px;
-  margin-top: -40px;
-  cursor: pointer;
-  z-index: 1;
-  -webkit-backface-visibility: hidden;
-  transition: transform 0.1s ease;
-}
-.owl-carousel .owl-video-play-icon:hover {
-  -ms-transform: scale(1.3, 1.3);
-  transform: scale(1.3, 1.3);
-}
-.owl-carousel .owl-video-playing .owl-video-play-icon,
-.owl-carousel .owl-video-playing .owl-video-tn {
-  display: none;
-}
-.owl-carousel .owl-video-tn {
-  opacity: 0;
-  height: 100%;
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  transition: opacity 0.4s ease;
-}
-.owl-carousel .owl-video-frame {
-  position: relative;
-  z-index: 1;
-  height: 100%;
-  width: 100%;
-}
-
-img {
-  max-width: 100%;
-  height: auto;
-}
-
-.owl-carousel .owl-nav .owl-prev {
-  float: left;
-}
-.owl-carousel .owl-nav .owl-next {
-  float: right;
-  background-position: right 0 !important;
-}
-.owl-theme .owl-nav [class*="owl-"]:hover {
-  background-color: transparent;
-}
-#stories .owl-nav [class*="owl-"] {
-  background: url("https://img.manuka.com.tr/Data/EditorFiles/tema/img/arrows2.png") !important;
-}
-#stories .owl-nav .owl-next {
-  background-position: right 0 !important;
-}
-.owl-theme .owl-nav {
-  margin: 0 !important;
-}
-.owl-carousel .owl-item img {
-  width: initial !important;
-}
 .owl-carousel .owl-nav .owl-prev {
   float: left;
 }
@@ -464,10 +269,63 @@ img {
   text-indent: -9999em;
   padding: 0;
   background-color: transparent;
-  background: url("https://img.manuka.com.tr/Data/EditorFiles/tema/img/arrows-2.png") !important;
+  background: url(https://img.manuka.com.tr/Data/EditorFiles/tema/img/arrows-2.png) !important;
   background-repeat: no-repeat;
   margin: 0;
   overflow: hidden;
   cursor: pointer;
+}
+.owl-theme .owl-nav [class*="owl-"] {
+  color: #fff;
+  font-size: 14px;
+  margin: 5px;
+  padding: 4px 7px;
+  background: #d6d6d6;
+  display: inline-block;
+  cursor: pointer;
+  border-radius: 3px;
+}
+.owl-carousel .owl-dot,
+.owl-carousel .owl-nav .owl-next,
+.owl-carousel .owl-nav .owl-prev {
+  cursor: pointer;
+  cursor: hand;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.owl-theme .owl-nav [class*="owl-"] {
+  width: 17px;
+  height: 32px;
+  text-indent: -9999em;
+  padding: 0;
+  background-color: transparent;
+  background: url(https://img.manuka.com.tr/Data/EditorFiles/tema/img/arrows-2.png) !important;
+  background-repeat: no-repeat;
+  margin: 0;
+  overflow: hidden;
+  cursor: pointer;
+}
+.owl-carousel .owl-nav .owl-next {
+  float: right;
+  background-position: right 0 !important;
+}
+.owl-theme .owl-dots,
+.owl-theme .owl-nav {
+  text-align: center;
+  -webkit-tap-highlight-color: transparent;
+}
+::selection {
+  background: #000;
+  color: #fff;
+}
+::-webkit-scrollbar-thumb {
+  background: #4d4d4d;
+  border-radius: 5px;
+}
+::-webkit-scrollbar-button {
+  display: none;
 }
 </style>
